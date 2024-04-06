8b389e4e6c17d537c87b7ff3d06a772345bfd5c7
lei <littlesomerest@gmail.com>
2024-04-06 10:31:31 +0800

    change img path

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ btnRoll.addEventListener('click', function () {
     const dice = Math.trunc(Math.random() * 6) + 1;
     // 2. Display dice
     diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+    diceEl.src = `./img/dice-${dice}.png`;
     // 3. Check for rolled 1: if true, swith player
     if (dice !== 1) {
       // Add dice to current score
